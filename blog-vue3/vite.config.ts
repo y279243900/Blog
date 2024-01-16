89244213235d87e49ce8e99e5a585fe7ebbacdb2
y279243900 <62531098+y279243900@users.noreply.github.com>
2024-01-17 04:02:01 +0800

    Update vite.config.ts

--- blog-vue3/vite.config.ts
+++ blog-vue3/vite.config.ts
@@ -1,13 +1,50 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+import { prismjsPlugin } from "vite-plugin-prismjs";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ["vue", "vue-router", "pinia"],
+      dts: "src/types/auto-imports.d.ts",
+    })
   ],
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹，process.cwd()为nodejs获取当前的绝对路径,resolve为拼接路径
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      // 指定symbolId格式
+      symbolId: "icon-[dir]-[name]",
+    }),
+    prismjsPlugin({
+      languages: [
+        "java",
+        "python",
+        "html",
+        "css",
+        "sass",
+        "less",
+        "go",
+        "cpp",
+        "c",
+        "js",
+        "ts",
+        "sql",
+        "bash",
+        "git",
+        "nginx",
+        "php",
+      ],
+      theme: "tomorrow",
+      css: true,
+    })，
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
